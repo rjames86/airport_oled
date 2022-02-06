2239ead8924013f4683773ce0263b0c5a995b7b6
Ryan M <ryan@ryanmo.co>
2022-02-06 13:03:53 -0700

    more tweaks

--- draw.py
+++ draw.py
@@ -56,7 +56,9 @@ class OLEDDraw:
         x = 0
         width, _ = self.font_large.getsize(display_text)
         # Start at negative half the screen so that the scrolling text starts at the middle of the screen
-        for i in range(0 - (self.width // 2), (width - self.width) + self.border, 3):
+        for i in range(0, (width - self.width) + self.border, 3):
+            if i == 0:
+                self.wait(0.5)
             self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
             self.draw_header(0, header)
             self.draw_body((x - i, self.top + 12), display_text)
